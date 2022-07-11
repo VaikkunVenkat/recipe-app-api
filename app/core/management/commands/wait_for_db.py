9b7f4dd0960dfb350c5c3d81083a0e251406158b
vaikkun-venkat <vaikkun.venkat@ipushpull.com>
2022-07-11 21:02:05 +0100

    fixed linting problems

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,6 +7,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """
     Django commad to wait for database
